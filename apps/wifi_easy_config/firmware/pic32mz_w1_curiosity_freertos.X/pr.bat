82b162f1e642dd17678dac0c384f5cb14616400a
zabooh <martin.ruppert@microchip.com>
2023-02-24 13:05:08 +0100

    ipecmdboost works when the pathes are set correctly

--- apps/wifi_easy_config/firmware/pic32mz_w1_curiosity_freertos.X/pr.bat
+++ apps/wifi_easy_config/firmware/pic32mz_w1_curiosity_freertos.X/pr.bat
@@ -1,8 +1,8 @@
-SET REL_FILE=.\dist\pic32mz_w1_curiosity_freertos\production\pic32mz_w1_curiosity_freertos.X.production
-SET TOOL="C:\Program Files\Microchip\xc32\v3.01\bin"
-SET GNU_BIN="C:\Program Files\Microchip\MPLABX\v6.05\gnuBins\GnuWin32\bin\"
-SET IPE_LOC="C:\Program Files\Microchip\MPLABX\v6.05\mplab_platform\mplab_ipe"
+SET IPE_DIR="C:\Program Files\Microchip\MPLABX\v6.05\mplab_platform\mplab_ipe"
+SET CUR_DIR="C:\Users\m91221\wolf\wireless_apps_pic32mzw1_wfi32e01\apps\wifi_easy_config\firmware\pic32mz_w1_curiosity_freertos.X" 
 
-java -jar %IPE_LOC%\ipecmd.jar -TPSNAP -P32MZ1025W104132 -M -F%REL_FILE%.hex -OL -OAS2
+rem java -jar %IPE_LOC%\ipecmd.jar -TPSNAP -P32MZ1025W104132 -M -F%REL_FILE%.hex -OL -OAS2
 
-
+cd %IPE_DIR%
+java -jar ipecmdboost.jar -TPSNAP -P32MZ1025W104132 -M -FC:\Users\m91221\wolf\wireless_apps_pic32mzw1_wfi32e01\apps\wifi_easy_config\firmware\pic32mz_w1_curiosity_freertos.X\dist\pic32mz_w1_curiosity_freertos\production\pic32mz_w1_curiosity_freertos.X.production.hex -OL -OAS2 -OY2012
+cd %CUR_DIR%
