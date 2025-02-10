10b0f47b439f506886f2b779f26cb11590e0dfed
zabooh <martin.ruppert@microchip.com>
2025-02-10 13:17:36 +0100

    FreeRTOS Repo location changed

--- apps/wifi_easy_config/git_clone_h3.bat
+++ apps/wifi_easy_config/git_clone_h3.bat
@@ -1,8 +1,9 @@
 set H3_INSTALL=../../../h3
 set H3_GITHUB="https://github.com/Microchip-MPLAB-Harmony"
 
+git clone -b "v3.8.5"               %H3_GITHUB%/mhc.git %H3_INSTALL%/mhc
 git clone -b "v3.15.0"              %H3_GITHUB%/bsp.git %H3_INSTALL%/bsp
-git clone -b "v10.4.6"              %H3_GITHUB%/CMSIS-FreeRTOS.git %H3_INSTALL%/CMSIS-FreeRTOS
+git clone -b "v10.4.6"              https://github.com/ARM-software/CMSIS-FreeRTOS.git %H3_INSTALL%/CMSIS-FreeRTOS
 git clone -b "v3.11.1"              %H3_GITHUB%/core.git %H3_INSTALL%/core
 git clone -b "v3.14.0"              %H3_GITHUB%/csp.git  %H3_INSTALL%/csp
 git clone -b "v3.7.0"               %H3_GITHUB%/wireless_wifi.git %H3_INSTALL%/wireless_wifi
